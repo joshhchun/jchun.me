b248d328a7465c57bb505de450462ab8303d19f3
joshhchun <jchun2@nd.edu>
2022-07-31 20:55:39 -0400

    update

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM node:14-alpine AS deps
+
+WORKDIR /usr/src/app
+COPY package.json yarn.lock ./
+RUN yarn install --frozen-lockfile
+
+FROM node:14-alpine AS BUILD_IMAGE
+WORKDIR /usr/src/app
+COPY --from=deps /app/node_modules ./node_modules
+COPY . .
+RUN yarn build
+
+CMD ["npm", "start"]
--- components/Second.tsx
+++ components/Second.tsx
@@ -2,20 +2,46 @@ import styles from "../styles/Home.module.css";
 import { Scene } from "react-scrollmagic";
 import { useInView } from "react-intersection-observer";
 
-const skills = [
+const languages = [
     {
-        type: "Python / Django",
+        type: "Python",
         level: 0.9,
         color: "rgba(156, 193, 230, 0.7)",
     },
     {
-        type: "Javascript / Express / Next",
-        level: 0.8,
+        type: "Javascript",
+        level: 0.9,
         color: "rgba(136, 169, 202, 0.7)",
     },
-    { type: "React", level: 0.8, color: "rgba(114, 142, 170, 0.7)" },
-    { type: "Docker", level: 0.75, color: "rgba(91, 113, 136, 0.7)" },
-    { type: "Java / Spring", level: 0.75, color: "rgba(73, 90, 109, 0.7)" },
+    { type: "C", level: 0.9, color: "rgba(73, 90, 109, 0.7)" },
+    {
+        type: "Typescript",
+        level: 0.81,
+        color: "rgba(136, 169, 202, 0.7)",
+    },
+    { type: "Java", level: 0.79, color: "rgba(73, 90, 109, 0.7)" },
+    { type: "C++", level: 0.79, color: "rgba(73, 90, 109, 0.7)" },
+    {
+        type: "PostgreSQL, MongoDB, MySQL",
+        level: 0.79,
+        color: "rgba(73, 90, 109, 0.7)",
+    },
+];
+
+const skills = [
+    {
+        type: "Python Django",
+        level: 0.9,
+        color: "rgba(156, 193, 230, 0.7)",
+    },
+    {
+        type: "Express.js / Next.js",
+        level: 0.9,
+        color: "rgba(136, 169, 202, 0.7)",
+    },
+    { type: "React.js", level: 0.82, color: "rgba(114, 142, 170, 0.7)" },
+    { type: "Docker", level: 0.82, color: "rgba(91, 113, 136, 0.7)" },
+    { type: "Java Spring Boot", level: 0.79, color: "rgba(73, 90, 109, 0.7)" },
 ];
 
 const Second = () => {
@@ -28,28 +54,32 @@ const Second = () => {
                 <Scene pin>
                     <div className={styles.background} id={styles.two}>
                         {inView && (
-                            <div className={styles.secondSkillsPage}>
+                            <>
                                 <div
                                     className={styles.middle}
                                     id={styles.middleTwo}
                                 >
                                     Skills
                                 </div>
-                                <div className={styles.skills}>
-                                    {skills.map((skill, index) => (
-                                        <>
-                                            <div className={styles.skill}>
-                                                <div className={styles.name}>
+                                <div className={styles.secondSkillsPage}>
+                                    <div className={styles.skills}>
+                                        <div>Languages</div>
+                                        {languages.map((skill, index) => (
+                                            <div
+                                                className={styles.skill}
+                                                key={index}
+                                            >
+                                                <div
+                                                    className={styles.name}
+                                                    key={skill.type}
+                                                >
                                                     {skill.type}
                                                 </div>
                                                 <div
                                                     className={
                                                         styles.levelContainer
                                                     }
-                                                    style={{
-                                                        ["--fadeoffset" as any]:
-                                                            index,
-                                                    }}
+                                                    key={skill.color}
                                                 >
                                                     <div
                                                         className={styles.level}
@@ -58,14 +88,54 @@ const Second = () => {
                                                                 skill.level,
                                                             ["--color" as any]:
                                                                 skill.color,
+                                                            ["--fadeoffset" as any]:
+                                                                index,
                                                         }}
+                                                        key={skill.level}
                                                     ></div>
                                                 </div>
                                             </div>
-                                        </>
-                                    ))}
+                                        ))}
+                                    </div>
+                                    <div className={styles.skills}>
+                                        <div>Frameworks</div>
+                                        {skills.map((skill, index) => (
+                                            <div
+                                                className={styles.skill}
+                                                key={index}
+                                            >
+                                                <div
+                                                    className={styles.name}
+                                                    key={skill.type}
+                                                >
+                                                    {skill.type}
+                                                </div>
+                                                <div
+                                                    className={
+                                                        styles.levelContainer
+                                                    }
+                                                    id={styles.frameworks}
+                                                    key={skill.color}
+                                                >
+                                                    <div
+                                                        className={styles.level}
+                                                        id={styles.frameworks}
+                                                        style={{
+                                                            ["--level" as any]:
+                                                                skill.level,
+                                                            ["--color" as any]:
+                                                                skill.color,
+                                                            ["--fadeoffset" as any]:
+                                                                index,
+                                                        }}
+                                                        key={skill.level}
+                                                    ></div>
+                                                </div>
+                                            </div>
+                                        ))}
+                                    </div>
                                 </div>
-                            </div>
+                            </>
                         )}
                     </div>
                 </Scene>
